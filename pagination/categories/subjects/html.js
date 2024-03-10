4de5753e5d8b97f94680629605bb40008afb91a6
ml-tweety <82256352+ml-tweety@users.noreply.github.com>
2024-03-10 19:03:09 +0530

    Add files via upload

---
+++ pagination/categories/subjects/html.js
@@ -0,0 +1,17 @@
+function pages(){
+    document.write(`
+    <div class="d-flex justify-content-center pagination">
+        <nav aria-label="pagination">
+            <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_01.html">1</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_02.html">2</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_03.html">3</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_04.html">4</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_05.html">5</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_06.html">6</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_07.html">7</a></li>
+            <li class="page-item"><a class="page-link" href="../../../../categories/subjects/computer/html/html_08.html">8</a></li>
+            </ul>
+        </nav> 
+    </div>   `);
+}
